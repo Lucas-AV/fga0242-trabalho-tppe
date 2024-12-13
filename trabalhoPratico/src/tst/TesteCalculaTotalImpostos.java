337223e0e8ebd635a6e91875364e8e0c2673e914
Pedro Barbosa <78980796+pedrobarbosaocb@users.noreply.github.com>
2024-12-12 22:09:32 -0300

    feat: TesteCalculaTotalImpostos.java

--- trabalhoPratico/src/tst/TesteCalculaTotalImpostos.java
+++ trabalhoPratico/src/tst/TesteCalculaTotalImpostos.java
@@ -1,38 +1,72 @@
 package tst;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import app.IRPF;
 
+@RunWith(Parameterized.class)
 public class TesteCalculaTotalImpostos {
     IRPF irpf;
-	
-	@Before
-	public void setup() {
-		irpf = new IRPF();
-        
+
+    // Parâmetros para o teste
+    private float expectedImpostos;
+    private float salario;
+    private float aluguel;
+    private float bolsa;
+    private float pensao;
+    private float deducaoIntegral;
+    private float contribuicao;
+
+    // Construtor para os parâmetros
+    public TesteCalculaTotalImpostos(float expectedImpostos, float salario, float aluguel, float bolsa, float pensao, float deducaoIntegral, float contribuicao) {
+        this.expectedImpostos = expectedImpostos;
+        this.salario = salario;
+        this.aluguel = aluguel;
+        this.bolsa = bolsa;
+        this.pensao = pensao;
+        this.deducaoIntegral = deducaoIntegral;
+        this.contribuicao = contribuicao;
+    }
+
+    // Método que fornece os dados para os testes
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            // expectedImpostos, salario, aluguel, bolsa, pensao, deducaoIntegral, contribuicao
+            {987.88f, 8000, 2000, 1500, 1500, 1000, 500}, // Cenário 1: Valores originais
+            {850.00f, 7000, 2000, 1500, 1200, 900, 400},   // Cenário 2: Valores alterados
+            {1100.50f, 9000, 2500, 0, 2000, 1500, 600}     // Cenário 3: Outro conjunto de valores
+        });
+    }
+
+    @Before
+    public void setup() {
+        irpf = new IRPF();
+
         // Cadastro dos rendimentos
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 8000);
-        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, 2000);
-        irpf.criarRendimento("Bolsa de estudos", IRPF.NAOTRIBUTAVEL, 1500);
-        
+        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, salario);
+        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, aluguel);
+        irpf.criarRendimento("Bolsa de estudos", IRPF.NAOTRIBUTAVEL, bolsa);
+
         // Cadastro do dependente Filho
         irpf.cadastrarDependente("filho", "filho");
-        irpf.cadastrarPensaoAlimenticia("filho", 1500); 
-        
+        irpf.cadastrarPensaoAlimenticia("filho", pensao);
+
         // Cadastro das deduções
-        irpf.cadastrarDeducaoIntegral("Contribuição previdenciária privada", 1000);
-        irpf.cadastrarContribuicaoPrevidenciaria(500);
+        irpf.cadastrarDeducaoIntegral("Contribuição previdenciária privada", deducaoIntegral);
+        irpf.cadastrarContribuicaoPrevidenciaria(contribuicao);
     }
 
     @Test
-	public void testeCalculaTotalImpostos() {
-        assertEquals(987.88f, irpf.calculaTotalImpostos(), 0f);
-	}
+    public void testeCalculaTotalImpostos() {
+        assertEquals(expectedImpostos, irpf.calculaTotalImpostos(), 0.01f);
+    }
 }
