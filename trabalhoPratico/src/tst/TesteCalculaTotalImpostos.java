fec0c529fcecde52e7024324f2ca707228a052ee
Pedro Barbosa <78980796+pedrobarbosaocb@users.noreply.github.com>
2024-12-13 00:59:23 +0000

    init: criando base para testeCalculaTotalImpostos

--- trabalhoPratico/src/tst/TesteCalculaTotalImpostos.java
+++ trabalhoPratico/src/tst/TesteCalculaTotalImpostos.java
@@ -11,5 +11,28 @@ import org.junit.Test;
 import app.IRPF;
 
 public class TesteCalculaTotalImpostos {
+    IRPF irpf;
+	
+	@Before
+	public void setup() {
+		irpf = new IRPF();
+        
+        // Cadastro dos rendimentos
+        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 8000);
+        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, 2000);
+        irpf.criarRendimento("Bolsa de estudos", IRPF.NAOTRIBUTAVEL, 1500);
+        
+        // Cadastro do dependente Filho
+        irpf.cadastrarDependente("filho", "filho");
+        irpf.cadastrarPensaoAlimenticia("filho", 1500); 
+        
+        // Cadastro das deduções
+        irpf.cadastrarDeducaoIntegral("Contribuição previdenciária privada", 1000);
+        irpf.cadastrarContribuicaoPrevidenciaria(500);
+    }
 
+    @Test
+	public void testeCalculaTotalImpostos() {
+        assertEquals(987.88f, irpf.calculaTotalImpostos(), 0f);
+	}
 }
